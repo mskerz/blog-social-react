9c789dbd0df51eff664759ad50d1da1a8a82e3bd
Mskerz <wichasin.s@gmail.com>
2025-06-05 03:22:22 +0700

    fix: remove error handling logic in AuthCallback component

--- src/page/auth/GoogleCallback.jsx
+++ src/page/auth/GoogleCallback.jsx
@@ -8,30 +8,25 @@ const AuthCallback = () => {
   const navigate = useNavigate();
   const location = useLocation();
   const dispatch = useDispatch();
-  
+
   useEffect(() => {
     // Parse the query parameters
     const params = new URLSearchParams(location.search);
     const token = params.get('token');
-    const error = params.get('error');
-    
+
     if (token) {
       // Store the token
       console.log(`Token with url params: ${token}`);
-      
+
       Cookies.set('accessToken', token);
       // Update Redux state
       dispatch({ type: 'auth/loginSuccess', payload: { accessToken: token } });
       // Navigate to profile or dashboard
       navigate('/profile');
-    } else if (error) {
-      // Navigate to login with error
-      console.log("have some thing ");
-      
-      navigate('/login', { state: { error } });
-    } 
+    }
+   
   }, [location, dispatch, navigate]);
-  
+
   return (
     <div className="min-h-screen flex items-center justify-center">
       <p>Processing authentication....</p>
